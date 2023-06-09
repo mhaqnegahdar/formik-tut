f4540444c24a4ffb7ec0fbff212533a0e033a278
Mohamad Haqnegahdar <104886566+maxjn@users.noreply.github.com>
2023-06-09 16:11:22 +0330

    Refactor Code: (#4)
    - added NewYoutubeForm component
    - refactored Form Code using Formik, Form, Field, ErrorMessage components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import YoutubeForm from "@/components/YoutubeForm";
+import NewYoutubeForm from "@/components/NewYoutubeForm";
 
 export default function Home() {
   return (
     <main className="flex items-center justify-center h-[100vh] bg-neutral-200 ">
-      <YoutubeForm />
+      <NewYoutubeForm />
     </main>
   );
 }
