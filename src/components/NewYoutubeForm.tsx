bf7c34d4dbbe69177777af09342a726a27c17f75
max jn <maxjn763@gmail.com>
2023-06-11 10:23:08 +0330

    Components: - added **p tag** to ErrorMessage component - added comments **Textarea** Field using as attribute - added address field using **render method** - added socials **nested object** field - added phoneNumber **ArrayField** and validation - used **FastField** for comments to stop rendering all the other fields

--- src/components/NewYoutubeForm.tsx
+++ src/components/NewYoutubeForm.tsx
@@ -1,13 +1,28 @@
 "use client";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import {
+  Formik,
+  Form,
+  Field,
+  ErrorMessage,
+  FieldProps,
+  FieldArray,
+  FastField,
+} from "formik";
 import Link from "next/link";
-import { object, string } from "yup";
+import { object, string, array } from "yup";
 
 // Types
 interface formData {
   name?: string;
   email?: string;
   channel?: string;
+  comments?: string;
+  address?: string;
+  socials?: {
+    linkedin?: string;
+    github?: string;
+  };
+  phoneNumbers?: string[];
 }
 
 // Initial form state
@@ -15,6 +30,13 @@ const initialValues = {
   name: "",
   email: "",
   channel: "",
+  comments: "",
+  address: "",
+  socials: {
+    linkedin: "",
+    github: "",
+  },
+  phoneNumbers: [""],
 };
 
 // Submission Functionality
@@ -22,10 +44,23 @@ const onSubmit = (values: formData) => {
   console.log("Form Submitted with values:", values);
 };
 
+const phoneRegex = RegExp(/09(0[1-2]|1[\d]|3[\d]|2[0-1])[\d]{3}[\d]{4}/g);
 const validationSchema = object({
   name: string().required("Required!"),
   email: string().email("Invalid email format!").required("Required"),
   channel: string().required("Required!"),
+  comments: string().required("Required!"),
+  address: string().required("Required!"),
+  socials: object({
+    linkedin: string().required("Required!"),
+    github: string().required("Required!"),
+  }),
+  phoneNumbers: array().of(
+    string()
+      .required("Required!")
+      .matches(phoneRegex, "Not a valid Iran phone number!")
+      .max(11, "Too Long!")
+  ),
 });
 
 const NewYoutubeForm = () => {
@@ -51,7 +86,11 @@ const NewYoutubeForm = () => {
               type="text"
               placeholder="Name"
             />
-            <ErrorMessage name="name" className="text-red-500 text-xs italic" />
+            <ErrorMessage
+              name="name"
+              className="text-red-500 text-xs italic"
+              component={"p"}
+            />
           </div>
 
           <div className="mb-4">
@@ -71,6 +110,7 @@ const NewYoutubeForm = () => {
             <ErrorMessage
               name="email"
               className="text-red-500 text-xs italic"
+              component={"p"}
             />
           </div>
 
@@ -91,9 +131,166 @@ const NewYoutubeForm = () => {
             <ErrorMessage
               name="channel"
               className="text-red-500 text-xs italic"
+              component={"p"}
             />
           </div>
 
+          {/* TEXTEREA && FastField */}
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="comments"
+            >
+              Comments
+            </label>
+            <FastField
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="comments"
+              name="comments"
+              placeholder="Comments"
+              as="textarea"
+            />
+            <ErrorMessage
+              name="comments"
+              className="text-red-500 text-xs italic"
+              component={"p"}
+            />
+          </div>
+
+          {/* RENDER METHOD */}
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="address"
+            >
+              Address
+            </label>
+            <Field name="address">
+              {({ field, meta, form }: FieldProps) => {
+                return (
+                  <>
+                    <input
+                      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                      id="address"
+                      type="text"
+                      placeholder="Address"
+                      {...field}
+                    />
+                    {meta.touched && meta.error && (
+                      <p className="text-red-500 text-xs italic">
+                        {meta.error}
+                      </p>
+                    )}
+                  </>
+                );
+              }}
+            </Field>
+          </div>
+
+          {/* Nested Object */}
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="linkedin"
+            >
+              Linkedin
+            </label>
+            <Field
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="linkedin"
+              name="socials.linkedin"
+              type="text"
+              placeholder="Linkedin"
+            />
+            <ErrorMessage
+              name="socials.linkedin"
+              className="text-red-500 text-xs italic"
+              component={"p"}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="github"
+            >
+              Github
+            </label>
+            <Field
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              id="github"
+              name="socials.github"
+              type="text"
+              placeholder="Github"
+            />
+            <ErrorMessage
+              name="socials.github"
+              className="text-red-500 text-xs italic"
+              component={"p"}
+            />
+          </div>
+
+          {/* Array Field */}
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="phoneNumbers"
+            >
+              Phone Numbers
+            </label>
+            <FieldArray name="phoneNumbers">
+              {({
+                push,
+                remove,
+                form: {
+                  values: { phoneNumbers },
+                },
+              }) => {
+                return phoneNumbers.map(
+                  (phoneNumber: string, index: number) => (
+                    <div className="mb-4" key={index}>
+                      <label
+                        className="block text-gray-700 text-sm font-bold mb-2"
+                        htmlFor={`phoneNumber[${index}]`}
+                      >
+                        Phone Number {index + 1}
+                      </label>
+                      <div className="flex items-center justify-between shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight  focus:shadow-outline">
+                        <Field
+                          id={`phoneNumber[${index}]`}
+                          type="text"
+                          name={`phoneNumbers[${index}]`}
+                          className="focus:outline-none"
+                        />
+                        <button
+                          type="button"
+                          onClick={() => push("")}
+                          className="text-xl text-neutral-500 font-semibold"
+                          title="Add phone"
+                        >
+                          +
+                        </button>
+                        {index != 0 && (
+                          <button
+                            type="button"
+                            onClick={() => remove(index)}
+                            className="text-xl text-neutral-500 font-semibold ml-2"
+                            title="Remove phone"
+                          >
+                            -
+                          </button>
+                        )}
+                      </div>
+                      <ErrorMessage
+                        name={`phoneNumbers[${index}]`}
+                        className="text-red-500 text-xs italic"
+                        component={"p"}
+                      />
+                    </div>
+                  )
+                );
+              }}
+            </FieldArray>
+          </div>
           <div className="flex items-center justify-between">
             <button
               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
